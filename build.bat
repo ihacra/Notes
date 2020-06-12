ffbf06d6a75b34c968dec3a35516b4efd162a8e5
Hacra <2199887379@qq.com>
2020-06-13 06:23:23 +0800

    生成静态网页

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 E:
 cd program_npe\Note\target\classes
 dir
-java -classpath E:\maven\repository\com\atlassian\commonmark\commonmark\0.15.1\commonmark-0.15.1.jar; com.hacra.note.Application
+java -classpath E:\maven\repository\com\atlassian\commonmark\commonmark\0.15.1\commonmark-0.15.1.jar; com.hacra.note.Application F:\Note
